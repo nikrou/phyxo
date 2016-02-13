3a59337a046c49293b14c8c50e52411e2b7f0cbb
Nicolas <nikrou77@gmail.com>
2016-02-13 13:48:53 +0100

    namespace issue

--- admin/site_manager.php
+++ admin/site_manager.php
@@ -1,7 +1,7 @@
 <?php
 // +-----------------------------------------------------------------------+
 // | Phyxo - Another web based photo gallery                               |
-// | Copyright(C) 2014-2015 Nicolas Roudaire         http://www.phyxo.net/ |
+// | Copyright(C) 2014-2016 Nicolas Roudaire         http://www.phyxo.net/ |
 // +-----------------------------------------------------------------------+
 // | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
 // | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
@@ -93,7 +93,6 @@ if (isset($_GET['action']) and isset($page['site'])) {
     if ($_GET['action']=='delete') {
         delete_site($page['site']);
         $page['infos'][] = $galleries_url.' '.l10n('deleted');
-        break;
     }
 }
 
--- src/Phyxo/DBLayer/mysqlConnection.php
+++ src/Phyxo/DBLayer/mysqlConnection.php
@@ -1,7 +1,7 @@
 <?php
 // +-----------------------------------------------------------------------+
 // | Phyxo - Another web based photo gallery                               |
-// | Copyright(C) 2014 Nicolas Roudaire              http://www.phyxo.net/ |
+// | Copyright(C) 2014-2016 Nicolas Roudaire         http://www.phyxo.net/ |
 // +-----------------------------------------------------------------------+
 // | This program is free software; you can redistribute it and/or modify  |
 // | it under the terms of the GNU General Public License version 2 as     |
@@ -30,7 +30,7 @@ class mysqlConnection extends DBLayer implements iDBLayer
     protected $db_link = null;
 
     public function db_connect($host, $user, $password, $database) {
-        if (($this->db_link = @mysql_connect($host, $user, $password)) === false) {
+        if (($this->db_link = @\mysql_connect($host, $user, $password)) === false) {
             throw new dbException('Can\'t connect to server');
         }
 
