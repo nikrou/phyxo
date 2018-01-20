5caafe81e13b37815aa12677b87623f4c40823a9
Nicolas <nikrou77@gmail.com>
2018-01-20 13:43:44 +0100

    New release with swiftmailer, inheritance for templates.

--- include/constants.php
+++ include/constants.php
@@ -11,7 +11,7 @@
 
 // Default settings
 if (!defined('PHPWG_VERSION')) { // can be defined in tests
-    define('PHPWG_VERSION', '1.7.0-dev');
+    define('PHPWG_VERSION', '1.7.0');
 }
 if (!defined('PHPWG_DEFAULT_LANGUAGE')) {
     define('PHPWG_DEFAULT_LANGUAGE', 'en_UK');
