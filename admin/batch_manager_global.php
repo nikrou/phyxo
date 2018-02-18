f36eef21a9bfa128e8ec8a629f71f4b40a5b9f4d
Nicolas <nikrou77@gmail.com>
2018-02-18 19:50:09 +0100

    Move LocalSiteReader to Phyxo namespace

--- admin/batch_manager_global.php
+++ admin/batch_manager_global.php
@@ -1,26 +1,13 @@
 <?php
-// +-----------------------------------------------------------------------+
-// | Phyxo - Another web based photo gallery                               |
-// | Copyright(C) 2014-2016 Nicolas Roudaire         http://www.phyxo.net/ |
-// +-----------------------------------------------------------------------+
-// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
-// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
-// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
-// +-----------------------------------------------------------------------+
-// | This program is free software; you can redistribute it and/or modify  |
-// | it under the terms of the GNU General Public License as published by  |
-// | the Free Software Foundation                                          |
-// |                                                                       |
-// | This program is distributed in the hope that it will be useful, but   |
-// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
-// | General Public License for more details.                              |
-// |                                                                       |
-// | You should have received a copy of the GNU General Public License     |
-// | along with this program; if not, write to the Free Software           |
-// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
-// | USA.                                                                  |
-// +-----------------------------------------------------------------------+
+/*
+ * This file is part of Phyxo package
+ *
+ * Copyright(c) Nicolas Roudaire  https://www.phyxo.net/
+ * Licensed under the GPL version 2.0 license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /**
  * Management of elements set. Elements can belong to a category or to the
@@ -31,6 +18,8 @@
 if (!defined('BATCH_MANAGER_BASE_URL')) {
     die('Hacking attempt!');
 }
+
+use Phyxo\LocalSiteReader;
 
 include_once(PHPWG_ROOT_PATH.'admin/include/functions.php');
 
@@ -420,7 +409,6 @@ $template->assign(
 );
 
 // metadata
-include_once( PHPWG_ROOT_PATH.'admin/site_reader_local.php');
 $site_reader = new LocalSiteReader('./'); // @TODO : in conf or somewhere else but no direct path here
 $used_metadata = implode(', ', $site_reader->get_metadata_attributes());
 
