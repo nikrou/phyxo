81947011133d5771c0cc3e0493b9d12c12e788e5
Nicolas <nikrou77@gmail.com>
2019-10-12 13:56:07 +0200

    Hack for old get_token function. When protocol is https token is under _csrf/https-authenticate

--- src/Phyxo/Functions/Utils.php
+++ src/Phyxo/Functions/Utils.php
@@ -407,7 +407,11 @@ class Utils
      */
     public static function get_token()
     {
-        return $_SESSION['_sf2_attributes']['_csrf/authenticate'];
+        if (!empty($_SESSION['_sf2_attributes']['_csrf/https-authenticate'])) {
+            return $_SESSION['_sf2_attributes']['_csrf/https-authenticate'];
+        } else {
+            return $_SESSION['_sf2_attributes']['_csrf/authenticate'];
+        }
     }
 
     /**
