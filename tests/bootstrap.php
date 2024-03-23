be5e7f809e17b0814a5c8c9e2e25e0c73a065d8c
Nicolas Roudaire <nikrou77@gmail.com>
2024-03-23 07:55:24 +0100

    Update symfony recipes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,13 +13,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-// Load cached env vars if the .env.local.php file exists
-// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include dirname(__DIR__) . '/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
-    (new Dotenv())->populate($env);
-} else {
-    // load all the .env files
-    (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
 $_SERVER += $_ENV;
