7a2430bca0dfb0b193b0ee7f17b153b1f8e60f38
Nicolas Roudaire <nikrou77@gmail.com>
2025-01-07 18:54:05 +0100

    category_view becomes album_view. Cope with #65

--- themes/treflez/src/js/grid-view.js
+++ themes/treflez/src/js/grid-view.js
@@ -1,43 +1,58 @@
-import 'jquery.cookie';
-import 'jquery-equalheights';
+import 'jquery.cookie'
+import 'jquery-equalheights'
 
 $(function () {
-    const cookie_name = 'category_view';
-    const cookie_params = { path: phyxo_root_url};
+  const cookie_name = 'album_view'
+  const cookie_params = { path: phyxo_root_url }
 
-    // Grid view button click
-    $('#btn-grid').click(function () {
-        if ($(this).hasClass('active')) {
-            return;
-        }
+  // Grid view button click
+  $('#btn-grid').click(function () {
+    if ($(this).hasClass('active')) {
+      return
+    }
 
-        $.cookie(cookie_name, 'grid', cookie_params);
-        $('#btn-grid').addClass('active');
-        $('#btn-list').removeClass('active');
-        $('#content')
-            .removeClass('content-list')
-            .addClass('content-grid')
-            .find('.col-outer').each(function () {
-                $(this).find('.card-body').attr('style', '');
-                $(this).find('a').addClass('d-block').find('.card-img-left').addClass('card-img-top').removeClass('card-img-left');
-                $(this).find('.card-body.list-view-only').addClass('d-none');
-                $(this).removeClass('col-12').addClass($(this).data('grid-classes'))
-            });
-    });
+    $.cookie(cookie_name, 'grid', cookie_params)
+    $('#btn-grid').addClass('active')
+    $('#btn-list').removeClass('active')
+    $('#content')
+      .removeClass('content-list')
+      .addClass('content-grid')
+      .find('.col-outer')
+      .each(function () {
+        $(this).find('.card-body').attr('style', '')
+        $(this)
+          .find('a')
+          .addClass('d-block')
+          .find('.card-img-left')
+          .addClass('card-img-top')
+          .removeClass('card-img-left')
+        $(this).find('.card-body.list-view-only').addClass('d-none')
+        $(this).removeClass('col-12').addClass($(this).data('grid-classes'))
+      })
+  })
 
-    // List view button click
-    $('#btn-list').click(function () {
-        if ($(this).hasClass('active')) {
-            return;
-        }
-        $.cookie(cookie_name, 'list', cookie_params);
-        $('#btn-list').addClass('active');
-        $('#btn-grid').removeClass('active');
-        $('#content')
-            .removeClass('content-grid').addClass('content-list').height('auto').find('.col-outer').each(function () {
-                $(this).find('a').removeClass('d-block').find('.card-img-top').addClass('card-img-left').removeClass('card-img-top');
-                $(this).find('.card-body.list-view-only').removeClass('d-none');
-                $(this).removeClass($(this).data('grid-classes')).addClass('col-12')
-            });
-    });
-});
+  // List view button click
+  $('#btn-list').click(function () {
+    if ($(this).hasClass('active')) {
+      return
+    }
+    $.cookie(cookie_name, 'list', cookie_params)
+    $('#btn-list').addClass('active')
+    $('#btn-grid').removeClass('active')
+    $('#content')
+      .removeClass('content-grid')
+      .addClass('content-list')
+      .height('auto')
+      .find('.col-outer')
+      .each(function () {
+        $(this)
+          .find('a')
+          .removeClass('d-block')
+          .find('.card-img-top')
+          .addClass('card-img-left')
+          .removeClass('card-img-top')
+        $(this).find('.card-body.list-view-only').removeClass('d-none')
+        $(this).removeClass($(this).data('grid-classes')).addClass('col-12')
+      })
+  })
+})
